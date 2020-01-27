c7e79539ca30b65a172829c317be8dba90ae499c
jayH4487 <>
2020-01-27 16:42:51 +0000

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+function Header() {
+    return (
+        <>
+            <header>
+                <img class="header-logo" src="https://www.gstatic.com/images/branding/product/1x/keep_48dp.png"></img>
+                <h2 class="header-title">Keep</h2>
+            </header>
+      </>
+    )
+}
+
+export default Header
