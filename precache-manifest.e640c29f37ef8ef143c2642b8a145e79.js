0e003c5993bd36622f1bfac4f8f28d2ce09531fd
jayH4487 <>
2020-01-30 10:10:43 +0000

    Updates

---
+++ precache-manifest.e640c29f37ef8ef143c2642b8a145e79.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ec927fdfe4d62712494250f721038d0",
+    "url": "/google-keep-clone-reactjs/index.html"
+  },
+  {
+    "revision": "bfc5df0e5567341c8444",
+    "url": "/google-keep-clone-reactjs/static/css/main.4927a188.chunk.css"
+  },
+  {
+    "revision": "ebefc019628dd838bf8d",
+    "url": "/google-keep-clone-reactjs/static/js/2.53f25844.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/google-keep-clone-reactjs/static/js/2.53f25844.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bfc5df0e5567341c8444",
+    "url": "/google-keep-clone-reactjs/static/js/main.3a04a502.chunk.js"
+  },
+  {
+    "revision": "9a3c7d88c611d4ed8db9",
+    "url": "/google-keep-clone-reactjs/static/js/runtime-main.fa82050c.js"
+  }
+]);
